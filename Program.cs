39c63d130d742b40c39de0734b8869d4483023de
daniilafendulov <daniilafendulov@bk.ru>
2020-09-14 02:48:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StrFragmentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int[] StrFragmentation(string str)
+        {
+
+        }
+    }
+}
